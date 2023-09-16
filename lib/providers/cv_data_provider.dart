7e581f0b4457b131f7ad329e1ba896c05d122835
Phosah <efosa.uyi@gmail.com>
2023-09-16 10:26:31 +0100

    cv view screen layout

--- lib/providers/cv_data_provider.dart
+++ lib/providers/cv_data_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cv_app/models/cv_data.dart';
 
 final cvDataProvider = StateProvider<CVData>(
   (ref) => CVData(
@@ -9,7 +10,7 @@ final cvDataProvider = StateProvider<CVData>(
         'I am a seasoned Frontend Developer with over three years of experience specializing in Javascript, Vue, and React. With a deep understanding of these technologies, I am adept at mastering new technologies swiftly. My curiosity and drive for innovation are continuously sparked by the dynamic and rapidly evolving world of web technologies.',
     workExperience: [
       WorkExperience(
-        date: '3 years of experience',
+        date: 'Oct 2022 - Present',
         company: 'Reftek',
         roles: [
           'Maintained a well-structured codebase, adhering to industry best practices and robust software architecture principles.',
@@ -17,7 +18,7 @@ final cvDataProvider = StateProvider<CVData>(
         ],
       ),
       WorkExperience(
-        date: '2 years of experience',
+        date: 'Jan 2020 - Oct 2022',
         company: 'Gruve',
         roles: [
           'Architected and implemented a scalable web3 application infrastructure using Next.js, leveraging its server-side rendering capabilities for optimal performance.',
@@ -27,31 +28,3 @@ final cvDataProvider = StateProvider<CVData>(
     ],
   ),
 );
-
-class CVData {
-  String fullName;
-  String slackName;
-  String githubHandle;
-  String personalBio;
-  List<WorkExperience> workExperience;
-
-  CVData({
-    required this.fullName,
-    required this.slackName,
-    required this.githubHandle,
-    required this.personalBio,
-    required this.workExperience,
-  });
-}
-
-class WorkExperience {
-  String date;
-  String company;
-  List<String> roles;
-
-  WorkExperience({
-    required this.date,
-    required this.company,
-    required this.roles,
-  });
-}
--- lib/screens/cv_view_screen.dart
+++ lib/screens/cv_view_screen.dart
@@ -1,28 +1,88 @@
-import 'package:cv_app/screens/cv_edit_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:cv_app/screens/cv_edit_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:cv_app/providers/cv_data_provider.dart';
 
-class CVViewScreen extends StatelessWidget {
+class CVViewScreen extends ConsumerWidget {
   const CVViewScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cvData = ref.watch(cvDataProvider);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CV APPLICATION'),
+        title: const Text('CV'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(12.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Text(
+              ' ${cvData.fullName}',
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+            ),
+            Text(
+              'Slack: ${cvData.slackName}',
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(
+              'Github Handle: ${cvData.githubHandle}',
+              style: const TextStyle(color: Colors.blue),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text('Personal Bio: ${cvData.personalBio}'),
+            const SizedBox(
+              height: 10,
+            ),
             const Text(
-              'This is the CV preview',
-              style: TextStyle(fontSize: 24),
+              'Work Experience:',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: cvData.workExperience.length,
+              itemBuilder: (context, index) {
+                final workExperience = cvData.workExperience[index];
+                return ListTile(
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        workExperience.company,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const Spacer(),
+                      Text(
+                        workExperience.date,
+                        style: const TextStyle(
+                            fontSize: 14, fontWeight: FontWeight.w500),
+                      ),
+                    ],
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children:
+                        workExperience.roles.map((role) => Text(role)).toList(),
+                  ),
+                );
+              },
             ),
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const CVEditScreen()),
+                  MaterialPageRoute(
+                    builder: (context) => const CVEditScreen(),
+                  ),
                 );
               },
               child: const Text('Edit'),
